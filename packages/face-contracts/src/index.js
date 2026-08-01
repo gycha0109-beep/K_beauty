e58eb3b8b5382342b23ebf20e00b00046d0916bc
gycha0109-beep <gycha0109@gmail.com>
2026-08-02 08:25:55 +0900

    feat(toolkit): [T3] export import contracts

--- packages/face-contracts/src/index.js
+++ packages/face-contracts/src/index.js
@@ -1,2 +1,6 @@
 export * from "./synthetic-generation/generation-spec.js";
 export * from "./synthetic-generation/compiled-prompt.js";
+export * from "./synthetic-import/import-errors.js";
+export * from "./synthetic-import/import-request.js";
+export * from "./synthetic-import/asset-record.js";
+export * from "./synthetic-import/candidate-manifest.js";
