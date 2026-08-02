e94146be16bead2ed91bfecb8f4def4950de3615
gycha0109-beep <gycha0109@gmail.com>
2026-08-03 00:32:00 +0900

    feat(toolkit): expose T8 reporting contracts

--- packages/face-contracts/src/index.js
+++ packages/face-contracts/src/index.js
@@ -8,3 +8,4 @@ export * from "./synthetic-observation/observation-contract.js";
 export * from "./synthetic-judgment/index.js";
 export * from "./synthetic-promotion/index.js";
 export * from "./synthetic-campaign/index.js";
+export * from "./synthetic-reporting/index.js";
