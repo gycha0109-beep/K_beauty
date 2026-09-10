81e8b2ce046d2621036a95e14c212741bff9a3fc
gycha0109-beep <gycha0109@gmail.com>
2026-09-10 10:39:01 +0900

    [DATA-SRC3] add product offers runtime seed

---
+++ tests/fixtures/product-offers/20260910102500_product_offers_runtime_seed.sql
@@ -0,0 +1,29 @@
+create table public.products (
+  id uuid primary key,
+  brand text,
+  name text,
+  price_min numeric,
+  price_max numeric,
+  buy_link text
+);
+
+grant select on public.products to service_role;
+
+insert into public.products(id, brand, name, price_min, price_max, buy_link)
+values
+  (
+    '93000000-0000-4000-8000-000000000001',
+    'ROUND LAB',
+    'Birch Juice Moisturizing Sun Cream',
+    18000,
+    22000,
+    'https://www.oliveyoung.co.kr/store/goods/getGoodsDetail.do?goodsNo=A000000001'
+  ),
+  (
+    '93000000-0000-4000-8000-000000000002',
+    'Beauty of Joseon',
+    'Relief Sun',
+    15000,
+    19000,
+    'https://www.hwahae.co.kr/goods/2002'
+  );
